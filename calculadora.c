421a5ba3f99ad7c824650a9f100db258f9907d24
vladimirleon <113052134+vladimirleon@users.noreply.github.com>
2022-09-07 13:06:46 -0500

    Update calculadora.c

--- calculadora.c
+++ calculadora.c
@@ -13,6 +13,12 @@ void resta() {
 }
 
 void mult() {
+    float n1=0, n2=0, res=0;
+    printf("Teclee dos números: /n");
+    scanf("%f", &n1);
+    scanf("%f", &n2);
+    res=n1*n2;
+    printf("El resultado de la multiplicacion es: %f", res);
 
 }
 
