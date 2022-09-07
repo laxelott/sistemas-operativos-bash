ff34691d6f690538c6424b8086e82c571766626f
EliotFntxy <55604359+EliotFntxy@users.noreply.github.com>
2022-09-07 13:07:07 -0500

    Update calculadora.c

--- calculadora.c
+++ calculadora.c
@@ -9,7 +9,14 @@ void suma() {
 }
 
 void resta() {
-
+    int a,b,res;
+    printf("Digite el primer numero\n");
+    scanf("%d",&a);
+    printf("Digite el segundo numero\n");
+    scanf("%d",&b);
+    res=a-b;
+    printf("El resultado es\n %d",res);
+ 
 }
 
 void mult() {
