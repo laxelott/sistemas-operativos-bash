3f941baf8898893f4cae917cc8d753992ebcc3a5
AT <laxelott@hotmail.com>
2022-09-07 18:05:47 +0000

    Agregar menu

--- calculadora.c
+++ calculadora.c
@@ -1,25 +1,64 @@
-int main () {
+int main()
+{
+    int continuar = 1;
+    int opcion;
+
+    while (continuar)
+    {
+
+        printf("--- Calculadora ---");
+        printf("1. Suma");
+        printf("2. Resta");
+        printf("3. Multiplicación");
+        printf("4. División");
+        printf("5. Creditos");
+        printf("6. Salir");
+        print("Escoga la opción:");
+
+        scanf("%d", &opcion);
+
+        switch (opcion)
+        {
+        case 1:
+            suma();
+            break;
+        case 2:
+            resta();
+            break;
+        case 3:
+            mult();
+            break;
+        case 4:
+            div();
+            break;
+        case 5:
+            creditos();
+            break;
+        case 6:
+            continuar = 0;
+            break;
+        }
+    }
+
     return 0;
 }
 
-
-
-void suma() {
-
+void suma()
+{
 }
 
-void resta() {
-
+void resta()
+{
 }
 
-void mult() {
-
+void mult()
+{
 }
 
-void div() {
-
+void div()
+{
 }
 
-void creditos() {
-    
+void creditos()
+{
 }
