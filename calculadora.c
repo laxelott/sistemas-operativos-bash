8f669e319559fbff6eedd4f789abb22854b4a803
frankorosenaimx <112896873+frankorosenaimx@users.noreply.github.com>
2022-09-07 13:07:10 -0500

    Update calculadora.c
    añadio la division

--- calculadora.c
+++ calculadora.c
@@ -17,8 +17,16 @@ void mult() {
 }
 
 void div(int a, int b) {
-    
-    z=a/b;
+    float n1, n2, division;
+    n1 = 0;
+    n2 = 0;
+    division = 0;
+    printf("Ingresa el numero:\n");
+    scanf("%f",&n1);
+    printf("Ingresa el otro numero:\n");
+    scanf("%f",&n2);
+    division = n1 / n2;
+    printf("El resultado es: %f",division);
 }
 
 void creditos() {
