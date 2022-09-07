652b57c2a4ad751779e5ed486b6b6cc29f96cfc2
Erik Alain Montiel Gómez <54964385+AladinMont@users.noreply.github.com>
2022-09-07 13:03:25 -0500

    Update calculadora.c

--- calculadora.c
+++ calculadora.c
@@ -5,7 +5,13 @@ int main () {
 
 
 void suma() {
-
+    float n1=0, n2=0, suma=0;
+    printf("Ingrese el primer numero:\n");
+    scanf("%f",&n1);
+    printf("Ingrese el segundo numero:\n");
+    scanf("%f",&n1);
+    suma = n1 + n2;
+    printf("El resultado de la suma es: %f",suma);
 }
 
 void resta() {
